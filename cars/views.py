20636e4afd8e3391499f1d3a4cd737d8d1fb3cbc
Nor sairi bin ahmad shahabudin <maxsim86@gmail.com>
2024-01-11 11:45:31 +0800

    initial commit

---
+++ cars/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import ReviewFrom
+
+# Create your views here.
+def rental_review(request):
+    
+    # POST REQUEST  --> FORM CONTENT --> THANK YOU
+    if request.method == 'POST':
+        form = ReviewFrom(request.POST)
+        # this is essentially django checking automatically things like, there are check  fields at forms.py
+        if form.is_valid():
+            
+            # {' first_name': 'sairi',}
+            print(form.cleaned_data)
+            #
+            return redirect(reverse('cars:thank_you'))
+        
+    #ELSE --> RENDER THE FORM 
+    else:
+        form = ReviewFrom()
+    return render(request, 'cars/rental_review.html', context={form:form} )
+    
+
+def thank_you(request):
+    return render(request, 'cars/thank_you.html') 
+
